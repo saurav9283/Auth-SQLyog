388c8ff96cadd5d2dfd2444982580ce67ab7db62
Saurav <sauravkumar00108@gmail.com>
2024-06-13 11:13:21 +0530

    refactor: Update saved service to check for existing item before saving
    The saved service has been updated to include a check for an existing item before saving it. This prevents duplicate items from being saved for a user. The check is performed by querying the `saved_item` table with the user ID and item ID. If a matching record is found, the service returns a message indicating that the item already exists in the user's saved list. If no matching record is found, the service proceeds with saving the item.
    
    This change improves the reliability and accuracy of the save functionality in the application.

--- routes/saved/saved.service.js
+++ routes/saved/saved.service.js
@@ -2,27 +2,38 @@ const pool = require('../../config/db.js');
 
 module.exports = {
     saveService: (data, callback) => {
-        const getItemQuery = process.env.GET_ITEM_BY_ID;
-        pool.query(getItemQuery, [data.itemId], (error, items) => {
+        const checkItemIdQuery = process.env.ITEMQUERY;
+        pool.query(checkItemIdQuery, [data.id, data.itemId], (error, results) => {
             if (error) {
                 console.log(error, "Error=====");
                 return callback(error);
             }
 
-            const saveItemQuery = process.env.SAVEITEM;
-            const values = items?.map(item => [data.id, item.name, item.imageUrl, item.description, item.rating]);
-            // console.log(values, "Values=====")
-
-            if (values.length > 0) {
-                pool.query(saveItemQuery, [values], (error, results) => {
+            if (results.length > 0) {
+                return callback(null, { message: 'Item with the given itemId already exists in saved_list for this user' });
+            } else {
+                const getItemQuery = process.env.GET_ITEM_BY_ID;
+                pool.query(getItemQuery, [data.itemId], (error, items) => {
                     if (error) {
                         console.log(error, "Error=====");
                         return callback(error);
                     }
-                    return callback(null, results);
+
+                    const saveItemQuery = process.env.SAVEITEM;
+                    const values = items?.map(item => [data.id, data.itemId, item.name, item.imageUrl, item.description, item.rating]);
+
+                    if (values && values.length > 0) {
+                        pool.query(saveItemQuery, [values], (error, results) => {
+                            if (error) {
+                                console.log(error, "Error=====");
+                                return callback(error);
+                            }
+                            return callback(null, results);
+                        });
+                    } else {
+                        return callback(null, { message: 'No items found with the given itemId' });
+                    }
                 });
-            } else {
-                return callback(null, { message: 'No items found with the given itemId' });
             }
         });
     }
